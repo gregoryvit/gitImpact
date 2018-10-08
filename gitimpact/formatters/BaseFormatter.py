ca4bb09becb5e00df69eb2bbf48fa023446c3c6a
Gregory Berngardt <gregoryvit@gmail.com>
2018-10-09 01:10:29 +0300

    Removed redundant files

--- gitimpact/formatters/BaseFormatter.py
+++
@@ -1,7 +0,0 @@
-
-class BaseFormatter(object):
-    def __init__(self, silent):
-        self.silent = silent
-
-    def format_tasks(self, graph):
-        return ""
--- gitimpact/formatters/__init__.py
+++
@@ -1,1 +0,0 @@
-from BaseFormatter import *
